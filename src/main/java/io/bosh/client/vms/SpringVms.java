02895a128fcce10e6c67b9786a588bf0fa4f0713
Johannes Hiemer <jhiemer@evoila.de>
2019-02-22 19:18:45 +0100

    chore(): updating SSHConfig and SpringVMs SSH handling

--- src/main/java/io/bosh/client/vms/SpringVms.java
+++ src/main/java/io/bosh/client/vms/SpringVms.java
@@ -15,6 +15,7 @@
  */
 package io.bosh.client.vms;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
@@ -23,7 +24,12 @@ import io.bosh.client.deployments.SSHConfig;
 import io.bosh.client.internal.AbstractSpringOperations;
 import io.bosh.client.tasks.Task;
 import io.bosh.client.tasks.Tasks;
+import org.springframework.web.client.RestOperations;
+import rx.Observable;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.security.KeyPair;
@@ -31,23 +37,17 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
-import org.springframework.http.HttpMethod;
-import org.springframework.web.client.RestOperations;
-
-import rx.Observable;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 /**
- * @author David Ehringer
+ * @author David Ehringer, Johannes Hiemer.
  */
 public class SpringVms extends AbstractSpringOperations implements Vms {
 
     private final ObjectMapper mapper = new ObjectMapper();
     private final Tasks tasks;
-    
+
     public SpringVms(RestOperations restOperations, URI root, Tasks tasks) {
         super(restOperations, root);
         this.tasks = tasks;
@@ -59,27 +59,27 @@ public class SpringVms extends AbstractSpringOperations implements Vms {
                 builder -> builder.pathSegment("deployments", deploymentName, "vms"))
                 .map(results -> Arrays.asList(results));
     }
-    
+
     @Override
     public Observable<List<Vm>> listDetails(String deploymentName) {
         return getEntity(Void.class, builder -> builder.pathSegment("deployments", deploymentName, "vms")
-                        .queryParam("format", "full"))
-            .flatMap(response -> tasks.trackToCompletion(getTaskId(response)))
-            .flatMap(task -> get(String.class, builder -> builder.pathSegment("tasks", task.getId(), "output")
-                                                           .queryParam("type", "result"))
-            .filter(rawDetails -> rawDetails != null)
-            .map(rawDetails -> rawDetails.split("\n"))
-            .map(rawDetails -> {
-                List<Vm> details = new ArrayList<Vm>();
-                for (String vm : rawDetails) {
-                    try {
-                        details.add(mapper.readValue(vm.getBytes(), Vm.class));
-                    } catch (IOException e) {
-                        throw new DirectorException("Unable to read VM data into VmDetails: " + vm, e);
-                    }
-                }
-                return details;
-             }));
+                .queryParam("format", "full"))
+                .flatMap(response -> tasks.trackToCompletion(getTaskId(response)))
+                .flatMap(task -> get(String.class, builder -> builder.pathSegment("tasks", task.getId(), "output")
+                        .queryParam("type", "result"))
+                        .filter(rawDetails -> rawDetails != null)
+                        .map(rawDetails -> rawDetails.split("\n"))
+                        .map(rawDetails -> {
+                            List<Vm> details = new ArrayList<Vm>();
+                            for (String vm : rawDetails) {
+                                try {
+                                    details.add(mapper.readValue(vm.getBytes(), Vm.class));
+                                } catch (IOException e) {
+                                    throw new DirectorException("Unable to read VM data into VmDetails: " + vm, e);
+                                }
+                            }
+                            return details;
+                        }));
     }
 
 
@@ -92,31 +92,54 @@ public class SpringVms extends AbstractSpringOperations implements Vms {
         }
         keyGen.initialize(1024);
         KeyPair keyPair =keyGen.generateKeyPair();
-        config = new SSHConfig(config, keyPair.getPublic().getEncoded().toString());
-        return this.ssh(config, keyPair.getPrivate().getEncoded().toString());
+        config = new SSHConfig(config, new String(keyPair.getPublic().getEncoded()));
+        return this.ssh(config, new String(keyPair.getPrivate().getEncoded()));
     }
 
-    public Observable<Session> ssh(SSHConfig config, String privateKey){
-        return exchangeWithTaskRedirect(config,
-                                        Task.class,
-                                        null,
-                                        HttpMethod.POST,
-                                        builder -> builder.pathSegment("deployments", config.getDeploymentName(), "ssh"))
-                     .map(exchange -> exchange.getBody())
-                     .map(body -> {
-                         List<Vm> vms = listDetails(config.getDeploymentName()).toBlocking().first();
-                         Vm vm = vms.get(config.getTarget().getIndexes());
+    private File writePrivateKeyFile(String privateKey) {
+        try {
+            String filename = String.valueOf(new Date().getTime());
+            File temp = File.createTempFile(filename, ".tmp");
 
-                         JSch jsch=new JSch();
-                         Session session = null;
-                         try {
-                             jsch.addIdentity(privateKey);
-                             session = jsch.getSession(config.getParams().getUser(), vm.getIps().get(0), 22);
-                         } catch (JSchException e) {
-                             throw new DirectorException("Unable to create ssh connection to " + vm.getJobName() + vm.getIndex(), e);
-                         }
-                         return session;
-                     });
+            BufferedWriter bw = new BufferedWriter(new FileWriter(temp));
+            bw.write(privateKey);
+            bw.close();
+
+            return temp;
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Observable<Session> ssh(SSHConfig config, String privateKey) {
+        return postForEntity(Task.class, config,
+                builder -> builder.pathSegment("deployments", config.getDeploymentName(), "ssh"))
+                .flatMap(response -> tasks.trackToCompletion(getTaskId(response)))
+                .map(body -> {
+                    Vm vm = listDetails(config.getDeploymentName()).toBlocking().first().stream().filter(filterVm -> {
+                        if (filterVm.getJobName().equals(config.getTarget().getJob()) &&
+                                filterVm.getIndex() == config.getTarget().getIndexes())
+                            return true;
+                        return false;
+                    }).findFirst().get();
+
+                    JSch jsch = new JSch();
+                    Session session = null;
+                    File privateKeyFile = null;
+                    try {
+                        privateKeyFile = this.writePrivateKeyFile(privateKey);
+                        jsch.addIdentity(privateKeyFile.getAbsolutePath());
+                        session = jsch.getSession(config.getParams().getUser(), vm.getIps().get(0), 22);
+                        session.setConfig("StrictHostKeyChecking", "no");
+                    } catch (JSchException e) {
+                        throw new DirectorException("Unable to create ssh connection to " + vm.getJobName() + vm.getIndex(), e);
+                    } finally {
+                        if (privateKeyFile.exists())
+                            privateKeyFile.delete();
+                    }
+                    return session;
+                });
     }
 
 }
